842328f70934e8287874137fabf6a26b24d4f093
kferg357 <kennethferguson90@gmail.com>
2021-06-20 22:52:44 -0400

    added to apps.js

--- app.js
+++ app.js
@@ -4,3 +4,17 @@ const table = require("console.table");
 const add = require("./lib/add");
 const update = require("./lib/update");
 const view = require("./lib/view");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3030,
+    user: "root",
+    password: "password"
+    database: "employee_db"
+});
+
+// connection.connect(function(err) {
+//     if (err) throw err;
+//     console.log("connected as id" + connection.threadId + "\n");
+//     exports.start();
+// });
