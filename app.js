8bc5114d095a7c433eb8c5062f5add060b57b41c
kferg357 <kennethferguson90@gmail.com>
2021-06-24 18:21:55 -0400

    added info

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
-const table = require("console.table");
-const add = require("./lib/add");
-const update = require("../lib/update");
-const view = require("../lib/view");
-
+// const table = require("console.table");
+// const add = require("./lib/add");
+// const update = require("../lib/update");
+// const view = require("../lib/view");
 const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -12,13 +11,11 @@ const connection = mysql.createConnection({
     password: "Kenndall2017!",
     database: "employee_db"
 });
-
 connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id" + connection.threadId + "\n");
     exports.start();
 });
-
 exports.start = () => {
     inquirer.prompt([
         {
@@ -30,7 +27,6 @@ exports.start = () => {
                 "Add Employee",
                 "Update Employee Role",
                 "EXIT",
-
             ]
         }
     ]).then(function(answer){
@@ -49,7 +45,6 @@ exports.start = () => {
         }
     });
 };
-
 
 
 
