9bb561850f7e30f35faaa4e6a5dd34175e361365
kferg357 <kennethferguson90@gmail.com>
2021-06-24 10:00:41 -0400

    trying to figure it out

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const mysql = require("mysql");
 const inquirer = require("inquirer");
 const table = require("console.table");
 const add = require("./lib/add");
-const update = require("./lib/update");
-const view = require("./lib/view");
+const update = require("../lib/update");
+const view = require("../lib/view");
 
 const connection = mysql.createConnection({
     host: "localhost",
